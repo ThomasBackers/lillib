23bb0820fbf00202639f28336163b13978910262
ThomasBackers <thomasbackers@gmail.com>
2021-10-01 14:58:54 +0200

    initial commit

---
+++ lillib.js
@@ -0,0 +1,47 @@
+export const rand = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
+export const randInt = (min, max) => {
+    return Math.random() * (max - min) + min;
+};
+
+export const randomRGBColor = () => {
+    return `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
+};
+
+export const invertRGBColor = color => {
+    const splittedColor = color.split(",");
+    const r = splittedColor[0].replace("rgb(", "");
+    const g = splittedColor[1].replace(" ", "");
+    const b = splittedColor[2].replace(" ", "").replace(")", "");
+    const rgb = [parseInt(r), parseInt(g), parseInt(b)];
+    for (let i = 0; i < rgb.length; i++) rgb[i] = (i === 3 ? 1 : 255) - rgb[i];
+    return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+};
+
+export const shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+};
+
+export const swapNodes = (node1, node2) => {
+    const afterNode2 = node2.nextElementSibling;
+    const parent = node2.parentNode;
+    node1.replaceWith(node2);
+    parent.insertBefore(node1, afterNode2);
+};
+
+export const delDuplicatedOf = parentNode => {
+    const nodesArray = [];
+    for (let node of parentNode.childNodes) if (node.nodeType === 1) nodesArray.push(node);
+    const seenTextContent = {};
+    for (let node of nodesArray) {
+        if (seenTextContent[node.textContent]) parentNode.removeChild(node);
+        else seenTextContent[node.textContent] = true;
+    }
+};
