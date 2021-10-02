dbc5f107f5d88427290f03680e85a105e89e54e9
ThomasBackers <thomasbackers@gmail.com>
2021-10-02 14:47:42 +0200

    began to doc & organize the script

--- lillib.js
+++ lillib.js
@@ -1,3 +1,6 @@
+//------------------------
+// MATHEMATICAL FUNCTIONS
+//------------------------
 export const rand = (min, max) => {
     return Math.random() * (max - min) + min;
 };
@@ -6,43 +9,10 @@ export const randInt = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
 };
 
-export const randDraw = (array, nb) => {
-    const draws = [];
-    for (let i = 0; i < nb; i++) draws.push(array[randInt(0, array.length)]);
-    return draws;
-};
-
-export const noRandDraw = (array, nb) => {
-    //if (nb > array.length) return RangeError("nb cannot be higher than array.length");
-    const draws = [];
-    let draw;
-    for (let i = 0; i < nb; i++)
-        do {
-            draw = array[randInt(0, array.length)];
-        } while (draws.includes(draw));
-        draws.push(draw);
-    return draws;
-};
-
-export const randomRGBColor = () => {
-    return `rgb(${randInt(0, 256)}, ${randInt(0, 256)}, ${randInt(0, 256)})`;
-};
-
-export const invertRGBColor = color => {
-    const splittedColor = color.split(",");
-    // returns ['rgb(r', ' g', ' b)']
-    // then we replace useless stuff with a void string
-    const r = splittedColor[0].replace("rgb(", ""),
-        g = splittedColor[1].replace(" ", ""),
-        b = splittedColor[2].replace(" ", "").replace(")", "");
-    // we also put them into an array to loop over it easily
-    const rgb = [parseInt(r), parseInt(g), parseInt(b)];
-    for (let i = 0; i < rgb.length; i++) rgb[i] = (i === 3 ? 1 : 255) - rgb[i];
-    return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
-};
-console.log(invertRGBColor("rgb(0,0,0)"));
-
-export const shuffleArray = array => {
+//------------------
+// ARRAYS FUNCTIONS
+//------------------
+export const shuffle = array => {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         const temp = array[i];
@@ -51,6 +21,28 @@ export const shuffleArray = array => {
     }
 };
 
+export const randDraw = (array, nb) => {
+    const draws = [];
+    for (let i = 0; i < nb; i++) draws.push(array[randInt(0, array.length)]);
+    return nb === 1 ? draws[0] : draws;
+};
+
+export const noRandDraw = (array, nb) => {
+    if (nb > array.length)
+        throw new RangeError("amout of unique draws (nb) cannot exceed the array length");
+    const draws = [];
+    let draw;
+    for (let i = 0; i < nb; i++)
+        do {
+            draw = array[randInt(0, array.length)];
+        } while (draws.includes(draw));
+    draws.push(draw);
+    return nb === 1 ? draws[0] : draws;
+};
+
+//-----------------
+// NODES FUNCTIONS
+//-----------------
 export const swapNodes = (node1, node2) => {
     const afterNode2 = node2.nextElementSibling;
     const parent = node2.parentNode;
@@ -58,7 +50,7 @@ export const swapNodes = (node1, node2) => {
     parent.insertBefore(node1, afterNode2);
 };
 
-export const delDuplicatedOf = parentNode => {
+export const delDuplNodes = parentNode => {
     const nodesArray = [];
     for (let node of parentNode.childNodes) if (node.nodeType === 1) nodesArray.push(node);
     const seenTextContent = {};
@@ -66,4 +58,24 @@ export const delDuplicatedOf = parentNode => {
         if (seenTextContent[node.textContent]) parentNode.removeChild(node);
         else seenTextContent[node.textContent] = true;
     }
+};
+
+//------------------
+// COLORS FUNCTIONS
+//------------------
+export const randRGBColor = () => {
+    return `rgb(${randInt(0, 256)}, ${randInt(0, 256)}, ${randInt(0, 256)})`;
+};
+
+export const invertRGBColor = color => {
+    const splittedColor = color.split(",");
+    // returns ['rgb(r', ' g', ' b)']
+    // then we replace useless stuff with an empty string
+    const r = splittedColor[0].replace("rgb(", ""),
+        g = splittedColor[1].replace(" ", ""),
+        b = splittedColor[2].replace(" ", "").replace(")", "");
+    // we also put them into an array to loop over it easily
+    const rgb = [parseInt(r), parseInt(g), parseInt(b)];
+    for (let i = 0; i < 3; i++) rgb[i] = (i === 3 ? 1 : 255) - rgb[i];
+    return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
 };
